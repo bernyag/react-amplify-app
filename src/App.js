8a197fe0412f39b67af4721c84e757c2c9c1d107
Bernardo <berny.ag30@gmail.com>
2021-02-13 23:56:52 -0600

    amplify backend

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to <code>Delta Tech</code>.
+          Welcome to Delta Tech.
         </p>
         <a
           className="App-link"
